f0c6443de10a8ab490d383569fffd7dea5e04ee3
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-02 14:53:24 -0300

    sales section

--- components/sections/Sale.tsx
+++ components/sections/Sale.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Sale = () => {
   return (
-    <div>
+    <div className="flex flex-col container items-center gap-8 pt-32 mx-auto relative">
+      <h2 className="text-4xl md:text-5xl font-bold text-center">
+        Limited collection <br /> for sale
+      </h2>
+      <p className="uppercase text-sm font-bold bg-gradient bg-clip-text text-transparent">
+        Discount up to 30%
+      </p>
+      <Link
+        href="#catalog"
+        className="w-36 flex flex-col items-center py-3 rounded-xl text-xs bg-gradient"
+      >
+        Buy keyboard
+      </Link>
     </div>
   );
 };
